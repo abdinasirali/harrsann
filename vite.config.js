cf207879ec244e1625b6294344bdfe44469f2e71
abdinasirali <134831256+abdinasirali@users.noreply.github.com>
2024-07-28 20:59:18 +0300

    hmm

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/harrsann/',  // beddel magaca_kaydkaaga magaca kaydkaaga dhabta ah
   server: {
     // port: 8001,
   },
